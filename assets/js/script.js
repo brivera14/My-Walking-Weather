3e6a30f186f6668bc1f2f53c48ddbecb92aa7b8a
Bryan Rivera Gonzalez <rivera.bryan1406@gmail.com>
2020-07-13 22:09:58 -0400

    icon added and some bootstrap css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+moment().format('L');
 /*-------------------------------------------------------------------------Declare Global Varibales---------------------------------------------------------------------------------*/
 var cityFormEl = document.querySelector("#city-form");
 var inputEl = document.querySelector("#input");
@@ -49,19 +51,45 @@ var displayCurrentWeather = function (response) {
 
     // clear old content
     weatherContainerEl.textContent = "";
-    
+    var mainDate = moment().format('L');
     // Display the information selecter from the API Response
     var titleEl = document.createElement("div");
     var cityEl = document.createElement("h3");
-    cityEl.textContent = response.name;
+    cityEl.textContent = response.name + " (" + mainDate + ")";
     var tempEl = document.createElement("p");
     tempEl.textContent = "Temperature: " + response.main.temp + "°F";
     var humEl = document.createElement("p");
     humEl.textContent = "Humidity: " + response.main.humidity + "%";
     var windSpeedEl = document.createElement("p");
     windSpeedEl.textContent = "Wind Speed: " + response.wind.speed + " MPH";
+    var weatherEl = response.weather[0].main;
+
+    if (weatherEl === "Rain") {
+        var iconEl = document.createElement('img')
+        iconEl.setAttribute("src", "http://openweathermap.org/img/wn/09d.png");
+        iconEl.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl=== "Clouds") {
+        var iconEl = document.createElement('img')
+        iconEl.setAttribute("src", "http://openweathermap.org/img/wn/03d.png");
+        iconEl.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl === "Clear") {
+        var iconEl = document.createElement('img')
+        iconEl.setAttribute("src", "http://openweathermap.org/img/wn/01d.png");
+        iconEl.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Drizzle") {
+        var iconEl = document.createElement('img')
+        iconEl.setAttribute("src", "http://openweathermap.org/img/wn/10d.png");
+        iconEl.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Snow") {
+        var iconEl = document.createElement('img')
+        iconEl.setAttribute("src", "http://openweathermap.org/img/wn/13d.png");
+        iconEl.setAttribute("style", "height: 60px; width: 60px");
+    }
     
     titleEl.appendChild(cityEl);
+    titleEl.appendChild(iconEl);
     titleEl.appendChild(tempEl);
     titleEl.appendChild(humEl);
     titleEl.appendChild(windSpeedEl);
@@ -92,17 +120,6 @@ var displayCurrentWeather = function (response) {
               }
         });
     });
-
-    //    // Get weather condition Icon
-    //    var icon = response.weather[0].icon;
-    //    var apiIconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    //    fetch(apiIconUrl).then(function(response){
-    //        response.then(function(dataIcon){
-    //           var iconEl = document.createElement("span");
-    //           iconEl.textContent = dataIcon;
-    //           titleEl.appendChild(iconEl);
-    //        });
-    //    });
 };
 /*--------------------------------------------------------------------------5-Day Forecast------------------------------------------------------------------------------------------*/
 var displayForecastWeather = function (response2) {
@@ -120,12 +137,38 @@ var displayForecastWeather = function (response2) {
     tempEl.textContent = "Temperature: " + response2.list[6].main.temp + "°F";
     var humEl = document.createElement("p");
     humEl.textContent = "Humidity: " + response2.list[6].main.humidity + "%";
-    
-    titleEl.appendChild(dateEl);
+    var weatherEl = response2.list[6].weather[0].main;
+
+    if (weatherEl === "Rain") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/09d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl=== "Clouds") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/03d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl === "Clear") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/01d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Drizzle") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/10d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Snow") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/13d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+    //titleEl.appendChild(dateEl);
+    titleEl.appendChild(forecastIcon);
     titleEl.appendChild(tempEl);
     titleEl.appendChild(humEl);
     
     forecastUno.appendChild(titleEl);
+    forecastUno.style.display = "";
 /*--------------------------------------------------------------------------2nd card------------------------------------------------------------------------------------------------*/
     // clear old content
     forecastDos.textContent = "";
@@ -138,12 +181,38 @@ var displayForecastWeather = function (response2) {
     tempEl.textContent = "Temperature: " + response2.list[14].main.temp + "°F";
     var humEl = document.createElement("p");
     humEl.textContent = "Humidity: " + response2.list[14].main.humidity + "%";
-    
-    titleEl.appendChild(dateEl);
+    var weatherEl = response2.list[14].weather[0].main;
+
+    if (weatherEl === "Rain") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/09d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl=== "Clouds") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/03d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl === "Clear") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/01d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Drizzle") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/10d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Snow") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/13d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+    //titleEl.appendChild(dateEl);
+    titleEl.appendChild(forecastIcon);
     titleEl.appendChild(tempEl);
     titleEl.appendChild(humEl);
     
     forecastDos.appendChild(titleEl);
+    forecastDos.style.display = "";
 /*--------------------------------------------------------------------------3rd card------------------------------------------------------------------------------------------------*/
     // clear old content
     forecastTres.textContent = "";
@@ -156,12 +225,38 @@ var displayForecastWeather = function (response2) {
     tempEl.textContent = "Temperature: " + response2.list[22].main.temp + "°F";
     var humEl = document.createElement("p");
     humEl.textContent = "Humidity: " + response2.list[22].main.humidity + "%";
-    
-    titleEl.appendChild(dateEl);
+    var weatherEl = response2.list[22].weather[0].main;
+
+    if (weatherEl === "Rain") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/09d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl=== "Clouds") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/03d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl === "Clear") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/01d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Drizzle") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/10d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Snow") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/13d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+    //titleEl.appendChild(dateEl);
+    titleEl.appendChild(forecastIcon);
     titleEl.appendChild(tempEl);
     titleEl.appendChild(humEl);
     
     forecastTres.appendChild(titleEl);
+    forecastTres.style.display = "";
 /*--------------------------------------------------------------------------4th card------------------------------------------------------------------------------------------------*/
     // clear old content
     forecastCuatro.textContent = "";
@@ -174,12 +269,38 @@ var displayForecastWeather = function (response2) {
     tempEl.textContent = "Temperature: " + response2.list[30].main.temp + "°F";
     var humEl = document.createElement("p");
     humEl.textContent = "Humidity: " + response2.list[30].main.humidity + "%";
-    
-    titleEl.appendChild(dateEl);
+    var weatherEl = response2.list[30].weather[0].main;
+
+    if (weatherEl === "Rain") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/09d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl=== "Clouds") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/03d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl === "Clear") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/01d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Drizzle") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/10d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Snow") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/13d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+    //titleEl.appendChild(dateEl);
+    titleEl.appendChild(forecastIcon);
     titleEl.appendChild(tempEl);
     titleEl.appendChild(humEl);
     
     forecastCuatro.appendChild(titleEl);
+    forecastCuatro.style.display = "";
 /*--------------------------------------------------------------------------5th card------------------------------------------------------------------------------------------------*/
     // clear old content
     forecastCinco.textContent = "";
@@ -192,17 +313,41 @@ var displayForecastWeather = function (response2) {
     tempEl.textContent = "Temperature: " + response2.list[38].main.temp + "°F";
     var humEl = document.createElement("p");
     humEl.textContent = "Humidity: " + response2.list[38].main.humidity + "%";
-    
-    titleEl.appendChild(dateEl);
+    var weatherEl = response2.list[38].weather[0].main;
+
+    if (weatherEl === "Rain") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/09d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl=== "Clouds") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/03d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    } else if (weatherEl === "Clear") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/01d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Drizzle") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/10d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+     else if (weatherEl === "Snow") {
+        var forecastIcon = document.createElement('img')
+        forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/13d.png");
+        forecastIcon.setAttribute("style", "height: 60px; width: 60px");
+    }
+    //titleEl.appendChild(dateEl);
+    titleEl.appendChild(forecastIcon);
     titleEl.appendChild(tempEl);
     titleEl.appendChild(humEl);
     
     forecastCinco.appendChild(titleEl);
+    forecastCinco.style.display = "";
 };
-
-/*--------------------------------------------------------------------------City Input and Store-----------------------------------------------------------------------------------*/
-
-var cityFormHandler = function (event) {
+/*--------------------------------------------------------------------------Event Listener------------------------------------------------------------------------------------------*/
+cityFormEl.addEventListener("submit", function (event) {
     event.preventDefault();
     var cityName = inputEl.value.trim();
     if (cityName) {
@@ -215,19 +360,15 @@ var cityFormHandler = function (event) {
         alert("Please enter a City Name");
     }
     displaySearchHistory();
-};
-
+});
 /*--------------------------------------------------------------------------Display Search History---------------------------------------------------------------------------------*/
-
 function displaySearchHistory () {
     var savedCity = JSON.parse(localStorage.getItem("City"));
     var cityList = document.createElement("div");
-    var cityBtn = document.createElement("button");
-    cityBtn.textContent = savedCity
+    var cityBtn = document.createElement("a");
+    cityBtn.setAttribute("href", "https://api.openweathermap.org/data/2.5/forecast?q=" + savedCity + "&units=imperial&APPID=0c8732c75eaaeabff3d4d751690ee9d5");
+    cityBtn.innerHTML = savedCity;
     cityList.appendChild(cityBtn);
     searchHistoryContainerEl.appendChild(cityList);
-    //cityFormEl.prepend(cityList);
+    
 }
-
-/*--------------------------------------------------------------------------Event Listener------------------------------------------------------------------------------------------*/
-cityFormEl.addEventListener("submit", cityFormHandler);
